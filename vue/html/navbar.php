77f8220558d6fb51b4230f9aff39ca6aada2b357
Fatma boudabbous <bdbbs2000@gmail.com>
2026-05-28 12:28:35 +0200

    organisation mvc

--- vue/html/navbar.php
+++ vue/html/navbar.php
@@ -2,7 +2,6 @@
 session_start();
 ?>
 
-
 <header>
     <nav class="navbar glass">
         <img class="img" src="../image/logo.png" alt="logo SweetLife" />
@@ -15,46 +14,32 @@ session_start();
             <li><a href="Profil.html">👤 Profil</a></li>
         </ul>
 
-
         <?php if (isset($_SESSION['user'])) : ?>
-
             <a class="btn-descop" href="../../controller/deconnexion.php">
                 <button class="btn-primary" type="button">
                     🔓 Déconnexion
                 </button>
             </a>
-
         <?php else : ?>
-
             <a class="btn-descop" href="inscription.html">
                 <button class="btn-primary" type="button">
                     S'inscrire
                 </button>
             </a>
-
         <?php endif; ?>
-
-
     </nav>
 
-
-
     <?php if (isset($_SESSION['user'])) : ?>
-
         <a class="btn-mobile btn-inscription-fixe" href="../../controller/deconnexion.php">
-            <button class="  btn-primary" type="button">
+            <button class="btn-primary" type="button">
                 🔓 Déconnexion
             </button>
         </a>
-
     <?php else : ?>
-
         <a class="btn-mobile btn-inscription-fixe" href="inscription.html">
             <button class="btn-primary" type="button">
                 S'inscrire
             </button>
         </a>
-
     <?php endif; ?>
-
 </header>
